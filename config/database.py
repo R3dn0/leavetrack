b2ed442c8d9b432e0f47c9313742367244f2cf14
R3dn0 <64706773+R3dn0@users.noreply.github.com>
2026-06-04 11:56:46 +0200

    chore: replace psycopg[binary] with psycopg2-binary

--- config/database.py
+++ config/database.py
@@ -1,11 +1,12 @@
-import psycopg
 import os
+
+import psycopg2
 from dotenv import load_dotenv
 
 load_dotenv()
 
 def get_connection():
-    return psycopg.connect(
+    return psycopg2.connect(
         host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT"),
         dbname=os.getenv("DB_NAME"),
