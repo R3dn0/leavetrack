3add126f22efbc11cd29635f1fa8f20dd99b459d
R3dn0 <64706773+R3dn0@users.noreply.github.com>
2026-06-08 10:15:56 +0200

    feat: add code field to AbsenceType, make leave_balance.total_days nullable

--- models/absence_type.py
+++ models/absence_type.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass, field
 @dataclass
 class AbsenceType:
     label: str
-    max_days_per_year: int
+    code: str
+    max_days_per_year: int | None
     is_paid: bool = field(default=False)
     id: int | None = field(default=None)
